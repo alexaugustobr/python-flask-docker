c182003d0349982b015086bafc19e51c937ed1a8
Alex Silva <alexaugustoti@gmail.com>
2023-12-03 18:34:27 -0300

    DEV

--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
 from flask import Flask,render_template
 import socket
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     try:
+        print(os.environ['HOME'])
+        print(os.environ['USER'])
+        print(os.environ['OS'])
+        print(os.environ['DB_NAME_TP'])
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
         return render_template('index.html', hostname=host_name, ip=host_ip)
